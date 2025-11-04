84e8124ed102862e8e79be98b1db3fbf44de29bf
DanielMouraoDev <danielmourao777@gmail.com>
2025-11-03 23:38:42 -0300

    feat: cria a classe filmesModel e atribui p Lombok a ele

--- src/main/java/com/MovieFlix/movieflix/filmesModel.java
+++ src/main/java/com/MovieFlix/movieflix/filmesModel.java
@@ -1,4 +1,13 @@
 package com.MovieFlix.movieflix;
 
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class filmesModel {
+
 }
---
+++ src/main/resources/V2_Add_filmes_tb_cadastro.sql
@@ -0,0 +1,1 @@
+ALTER TABLE tb_cadastro_de_filmes
