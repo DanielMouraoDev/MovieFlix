932d1a0de1a4929148194433db28257b981bb10f
DanielMouraoDev <danielmourao777@gmail.com>
2025-11-26 16:44:47 -0300

    feat: cria o Package Entity e a classe Category

--- src/main/java/com/MovieFlix/movieflix/filmesModel.java
+++ src/main/java/com/MovieFlix/movieflix/filmesModel.java
@@ -1,13 +1,4 @@
 package com.MovieFlix.movieflix;
-
-import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 public class filmesModel {
 
 }
