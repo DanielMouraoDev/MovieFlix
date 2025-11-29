0d32cf7891d58ba3c6a1045b0d959da40eb871a2
DanielMouraoDev <danielmourao777@gmail.com>
2025-11-29 03:36:08 -0300

    refact: refatora os nomes das migrations

--- src/main/java/com/MovieFlix/movieflix/Entity/movie.java
+++ src/main/java/com/MovieFlix/movieflix/Entity/movie.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 @NoArgsConstructor
@@ -21,20 +22,37 @@ public class movie {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String title;
 
     private String description;
 
-    @Column (name = "release_date")
+    @Column(name = "release_date")
     private LocalDate releaseDate;
 
     private double rating;
 
     @CreationTimestamp
-    @Column (name = "created_at")
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
-    @Column (name = "updated_at")
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    @ManyToMany
+    @JoinTable(
+            name = "movie_category",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private List<category> categories; // Importante: Usa a SUA entidade 'category'
+
+    @ManyToMany
+    @JoinTable(
+            name = "movie_streaming",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "streaming_id")
+    )
+    private List<streaming> streamings; // Importante: Usa a SUA entidade 'streaming'
 }
