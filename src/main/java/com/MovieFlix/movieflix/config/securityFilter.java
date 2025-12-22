bb12001a6f1b50c8dc145d0ada03e005e95ecc61
DanielMouraoDev <danielmourao777@gmail.com>
2025-12-22 19:13:40 -0300

    feat: implemento o Token

--- src/main/java/com/MovieFlix/movieflix/config/securityFilter.java
+++ src/main/java/com/MovieFlix/movieflix/config/securityFilter.java
@@ -1,48 +1,50 @@
 package com.MovieFlix.movieflix.config;
 
-import com.MovieFlix.movieflix.Entity.user;
-import com.MovieFlix.movieflix.Repository.userRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections; // <--- Importação necessária adicionada
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class securityFilter extends OncePerRequestFilter {
 
     private final tokenService tokenService;
-    private final userRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = this.recoverToken(request);
+        String authorizationHeader = request.getHeader("Authorization");
 
-        if (token != null) {
-            var login = tokenService.validateToken(token);
+        if (Strings.isNotEmpty(authorizationHeader) && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring("Bearer ".length());
 
-            if(login != null){
-                user user = userRepository.findByEmail(login).orElse(null);
+            Optional<JWTUserData> optJWTUserData = tokenService.verifyToken(token);
 
-                if (user != null) {
-                    var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                }
+            if(optJWTUserData.isPresent()) {
+                JWTUserData UserData = optJWTUserData.get();
+
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        UserData,
+                        null,
+                        Collections.emptyList() 
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
-        }
-        filterChain.doFilter(request, response);
-    }
 
-    private String recoverToken(HttpServletRequest request) {
-        var authHeader = request.getHeader("Authorization");
-        if (authHeader == null) return null;
-        return authHeader.replace("Bearer ", "");
+            filterChain.doFilter(request, response);
+        } else {
+            filterChain.doFilter(request, response);
+        }
     }
 }
--- src/main/java/com/MovieFlix/movieflix/config/tokenService.java
+++ src/main/java/com/MovieFlix/movieflix/config/tokenService.java
@@ -2,12 +2,14 @@ package com.MovieFlix.movieflix.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTVerificationException; // <--- Importação necessária
 import com.MovieFlix.movieflix.Entity.user;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.Optional;
 
 @Component
 public class tokenService {
@@ -28,16 +30,23 @@ public class tokenService {
                 .sign(algorithm);
     }
 
-    public String validateToken(String token) {
-        try {
-            Algorithm algorithm = Algorithm.HMAC256(secret);
-            return JWT.require(algorithm)
-                    .withIssuer("API Movieflix")
-                    .build() // Agora o build() vai funcionar
-                    .verify(token)
-                    .getSubject();
-        } catch (JWTVerificationException exception) {
-            return "";
-        }
+    public Optional<JWTUserData>verifyToken(String token) {
+    try {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        DecodedJWT jwt = JWT.require(algorithm)
+                .build()
+                .verify(token);
+
+        return Optional.of(JWTUserData
+                .builder()
+                .id(jwt.getClaim("userId").asLong())
+                .name(jwt.getClaim("name").asString())
+                .email(jwt.getSubject())
+                .build());
+    } catch (JWTVerificationException ex) {
+        return Optional.empty();
     }
+    }
+
 }
